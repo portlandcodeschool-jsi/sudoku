6acda04bafa5c00ce50c98f1a886d62da67892b0
Erin Call <hello@erincall.com>
2015-06-16 14:39:20 -0700

    Tested solver that can handle simple puzzles

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 'use strict';
 var _ = require('lodash');
 
-module.exports.buildColumns = function(cells) {
+var buildColumns = module.exports.buildColumns = function(cells) {
   var i,
       j,
       matrix = [[], [], [], [], [], [], [], [], []];
@@ -14,9 +14,9 @@ module.exports.buildColumns = function(cells) {
   }
 
   return matrix;
-}
+};
 
-module.exports.buildRows = function(cells) {
+var buildRows = module.exports.buildRows = function(cells) {
   var i,
       j,
       matrix = [[], [], [], [], [], [], [], [], []];
@@ -27,9 +27,9 @@ module.exports.buildRows = function(cells) {
   }
 
   return matrix;
-}
+};
 
-module.exports.buildGroups = function(cells) {
+var buildGroups = module.exports.buildGroups = function(cells) {
   var group,
       groupPosition,
       row,
@@ -51,8 +51,83 @@ module.exports.buildGroups = function(cells) {
   }
 
   return matrix;
-}
+};
 
-module.exports.solve = function() {
-  // TODO
+var Cell = module.exports.Cell = function Cell(initial) {
+  this.row = null;
+  this.column = null;
+  this.group = null;
+  this.value = null;
+  this.solved = false;
+  if (isNaN(Number(initial))) {
+    this.possibilities = _.range(1, 10);
+  } else {
+    this.possibilities = [Number(initial)];
+  }
+  this.checkForSolved();
+};
+
+Cell.prototype.checkForSolved = function() {
+  if (this.possibilities.length === 1) {
+    this.solved = true;
+    this.value = this.possibilities[0];
+  }
+};
+
+Cell.prototype.applyRestrictions = function() {
+  var changedAnything = false;
+
+  if (! this.solved) {
+    ['row', 'column', 'group'].forEach(function(grouping) {
+      this[grouping].forEach(function(otherCell) {
+        if (otherCell.solved) {
+          if (this.possibilities.indexOf(otherCell.value) !== -1) {
+            this.possibilities.splice(this.possibilities.indexOf(otherCell.value), 1);
+            this.checkForSolved();
+            changedAnything = true;
+          }
+        }
+      }.bind(this));
+    }.bind(this));
+  }
+
+  return changedAnything;
+};
+
+var populateReferences = module.exports.populateReferences = function populateReferences(cells, rows, columns, groups) {
+  rows.forEach(function (row) {
+    row.forEach(function(cell) {
+      cell.row = row;
+    });
+  });
+  columns.forEach(function (column) {
+    column.forEach(function(cell) {
+      cell.column = column;
+    });
+  });
+  groups.forEach(function (group) {
+    group.forEach(function(cell) {
+      cell.group = group;
+    });
+  });
+};
+
+module.exports.solve = function(board) {
+  var cells = board.split('').map(function(initial) {return new Cell(initial);});
+  var rows = buildRows(cells);
+  var columns = buildColumns(cells);
+  var groups = buildGroups(cells);
+
+  populateReferences(cells, rows, columns, groups);
+
+  var changedAnything = false;
+  function applyRestrictions(cell) {
+    changedAnything = (cell.applyRestrictions() || changedAnything);
+  }
+  do {
+    changedAnything = false;
+    cells.forEach(applyRestrictions);
+  } while (changedAnything);
+
+  return cells;
 };
--- test/sudoku_test.js
+++ test/sudoku_test.js
@@ -1,6 +1,7 @@
 /* jshint node:true, mocha: true */
 
 'use strict';
+var _ = require('lodash');
 var sudoku = require('../index');
 var expect = require('chai').expect;
 
@@ -69,3 +70,119 @@ describe('break a board into groups', function() {
     expect(sudoku.buildGroups(cells)).to.eql(matrix);
   });
 });
+
+describe('Cell', function() {
+  it ('initializes its possibilities', function() {
+    var cell = new sudoku.Cell('.');
+    expect(cell.possibilities).to.eql([1, 2, 3, 4, 5, 6, 7, 8, 9]);
+    expect(cell.solved).to.equal(false);
+    expect(cell.value).to.equal(null);
+  });
+
+  it('initializes as solved', function() {
+    var cell = new sudoku.Cell('8');
+    expect(cell.possibilities).to.eql([8]);
+    expect(cell.solved).to.equal(true);
+    expect(cell.value).to.equal(8);
+  });
+
+  describe('checkForSolved', function() {
+    it('notes the cell is solved', function() {
+      var cell = new sudoku.Cell('.');
+      cell.possibilities = [1];
+      cell.checkForSolved();
+      expect(cell.solved).to.equal(true);
+    });
+
+    it('sets the value poperty', function() {
+      var cell = new sudoku.Cell('.');
+      cell.possibilities = [1];
+      cell.checkForSolved();
+      expect(cell.value).to.equal(1);
+    });
+  });
+
+  describe('applyRestrictions', function() {
+    beforeEach(function() {
+      this.cell = new sudoku.Cell('.');
+      this.cell.possibilities = [1, 2, 3];
+      this.cell.row = [];
+      this.cell.column = [];
+      this.cell.group = [];
+    });
+
+    it('reduces possibilities based on solved cells in its row', function() {
+      var otherCell = new sudoku.Cell('2');
+      this.cell.row = [otherCell];
+
+      this.cell.applyRestrictions();
+
+      expect(this.cell.possibilities).to.eql([1, 3]);
+    });
+
+    it('reduces possibilities based on solved cells in its column', function() {
+      var otherCell = new sudoku.Cell('2');
+      this.cell.column = [otherCell];
+
+      this.cell.applyRestrictions();
+
+      expect(this.cell.possibilities).to.eql([1, 3]);
+    });
+
+    it('reduces possibilities based on solved cells in its group', function() {
+      var otherCell = new sudoku.Cell('2');
+      this.cell.group = [otherCell];
+
+      this.cell.applyRestrictions();
+
+      expect(this.cell.possibilities).to.eql([1, 3]);
+    });
+  });
+});
+
+describe('populateReferences', function() {
+  beforeEach(function() {
+    var cells = this.cells = [
+      new sudoku.Cell('.'),
+      new sudoku.Cell('.'),
+      new sudoku.Cell('.'),
+      new sudoku.Cell('.')
+    ];
+
+    var rows = this.rows = [[cells[0], cells[1]], [cells[2], cells[3]]];
+    var columns = this.columns = [[cells[0], cells[2]], [cells[1], cells[3]]];
+    var groups = this.groups = [[cells[0]], [cells[1]], [cells[2]], [cells[3]]];
+
+    sudoku.populateReferences(cells, rows, columns, groups);
+  });
+
+  it('sets up circular references to the rows', function() {
+    expect(this.cells[0].row).to.equal(this.rows[0]);
+    expect(this.cells[1].row).to.equal(this.rows[0]);
+    expect(this.cells[2].row).to.equal(this.rows[1]);
+    expect(this.cells[3].row).to.equal(this.rows[1]);
+  });
+
+  it('sets up circular references to the columns', function() {
+    expect(this.cells[0].column).to.equal(this.columns[0]);
+    expect(this.cells[1].column).to.equal(this.columns[1]);
+    expect(this.cells[2].column).to.equal(this.columns[0]);
+    expect(this.cells[3].column).to.equal(this.columns[1]);
+  });
+
+  it('sets up circular references to the groups', function() {
+    expect(this.cells[0].group).to.equal(this.groups[0]);
+    expect(this.cells[1].group).to.equal(this.groups[1]);
+    expect(this.cells[2].group).to.equal(this.groups[2]);
+    expect(this.cells[3].group).to.equal(this.groups[3]);
+  });
+});
+
+describe('solve', function() {
+  it('solves the board', function() {
+    var simpleBoard = '158.2..6.2...8..9..3..7.8.2.6.74......4.6.7......19.5.4.9.3..2..2..5...8.7..9.413';
+    var solved = sudoku.solve(simpleBoard);
+    expect(_.all(solved.map(function(cell) {return cell.solved;}))).to.equal(true);
+  });
+});
+
